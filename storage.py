8550a9a11a5d07ad5b7902015e0f12c07ffea6b9
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2022-11-13 17:49:02 +0400

    Fix storage loading

--- storage.py
+++ storage.py
@@ -23,9 +23,9 @@ class Storage(metaclass=Singleton):
         self._file = file
         with open(file, 'a+') as storage_file:
             storage_file.seek(0)
-            entries = json.load(storage_file)
+            contents = storage_file.read()
+            entries = [] if len(contents) == 0 else json.loads(storage_file)
 
-            pprint(entries)
             for entry in entries:
                 self.entries.append(from_dict(Entry, entry))
 
